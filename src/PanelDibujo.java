a1c89db73596ed3e8f281625ec2d172c27519f4f
Elian027 <elian.moreira@epn.edu.ec>
2023-01-17 11:50:37 -0500

    IMC

--- src/PanelDibujo.java
+++ src/PanelDibujo.java
@@ -6,9 +6,9 @@ public class PanelDibujo extends JPanel {
         super.paintComponent(g); //Llama a paintComponent para que el panel se muestre correctamente
         int ancho = getWidth();
         int alto = getHeight();
-        g.drawLine(110,50,110,300); //dibuja linea desde esquina superior izquierda a esquina inferior derecha
-        g.drawLine(110,50,300,50);//dibuja linea desde esquina inferior izquierda a superior derecha
-        g.drawLine(110,180,270,180);
-        g.drawLine(110,300,300,300);
+        g.draw3DRect(50,10,75,25, true); //dibuja linea desde esquina superior izquierda a esquina inferior derecha
+        g.draw3DRect(50,100,75,25, true);//dibuja linea desde esquina inferior izquierda a superior derecha
+        g.draw3DRect(25,10,25,115, true);
+        g.draw3DRect(50,55,55,25, true);
     }
 }
---
+++ src/v1.java
@@ -0,0 +1,50 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.DecimalFormat;
+
+public class v1 {
+    private JPanel panel1;
+    private JTextField peso;
+    private JTextField altura;
+    private JButton calcularButton;
+    private JButton cancelarButton;
+    private JLabel respuesta;
+
+    public v1() {
+        cancelarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null,"Gracias por usar mi programa");
+            }
+        });
+        calcularButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                DecimalFormat dfr = new DecimalFormat("#.00");
+                double a = Double.parseDouble(peso.getText());
+                double b = Double.parseDouble(altura.getText());
+                double res = (a/Math.pow(b,2));
+                String msj = "Su IMC es -> "+dfr.format(res);
+                respuesta.setText(msj);
+                //String msj = "Su IMC es -> "+res;
+                //JOptionPane.showMessageDialog(null,msj+res);
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("v1");
+        frame.setContentPane(new v1().panel1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+
+    }
+    public void setPeso (JTextField peso) {
+        this.peso = peso;
+    }
+    public void setAltura (JTextField altura) {
+        this.altura = altura;
+    }
+}
